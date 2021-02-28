5b9ac69686160d544e7834cd3d56be7ef9e982ca
wata909 <wata909@gmail.com>
2021-02-28 10:36:01 +0900

    change map tiles

--- docs/habs_map.js
+++ docs/habs_map.js
@@ -24,11 +24,11 @@ function init() {
     map.div.style.backgroundColor = 'rgb(255,255,255)';
 
     // 迅速測図
-    var rapid = new OpenLayers.Layer.TMS(
-//    var rapid = new OpenLayers.Layer.XYZ(
+//    var rapid = new OpenLayers.Layer.TMS(
+    var rapid = new OpenLayers.Layer.XYZ(
         "迅速測図（1880年代）",
-        "https://aginfo.cgk.affrc.go.jp/ws/tmc/1.0.0/Kanto_Rapid-900913/",
-//        "https://aginfo.cgk.affrc.go.jp/ws/tmc/1.0.0/Kanto_Rapid-900913-L/${z}/${x}/${y}.png"
+//        "https://habs.dc.affrc.go.jp/rapid16/",
+        "https://aginfo.cgk.affrc.go.jp/ws/tmc/1.0.0/Kanto_Rapid-900913-L/${z}/${x}/${y}.png",
 
         {
 //            "getURL": getUrlByXYZ,
